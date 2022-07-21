404f2e5f217615faa4cebf75d99824bc03c83895
Ben Dalling <ben.dalling@6point6.co.uk>
2022-07-21 11:55:11 +0100

    new: dev: Add tests for checking Python file size.

--- tests/step_defs/test_issue21.py
+++ tests/step_defs/test_issue21.py
@@ -1,12 +1,9 @@
 """Fix Issue 21 feature tests."""
 
-from pytest_bdd import scenario
+from pytest_bdd import scenarios
+
+scenarios('../features/issue21.feature')
 
 # Ensure that the PyTest fixtures provided in testinfra-bdd are available to
 # your test suite.
 pytest_plugins = ['testinfra_bdd']
-
-
-@scenario('../features/issue21.feature', 'Issue 21')
-def test_issue_21():
-    """Issue 21."""
